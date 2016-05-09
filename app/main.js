2eaf6d735374506305c486c10ee0d58be225b58d
Dan Wendorf <dwendorf@pivotal.io>
2016-05-08 23:41:13 -0700

    Auto-refresh every 30 seconds

--- app/main.js
+++ app/main.js
@@ -1,13 +1,24 @@
 var axios = require('axios');
 
-axios.get('api/v1/checks?checkfile=https://raw.githubusercontent.com/cloudfoundry/capi-checkman/master/pipeline').then(function(response) {
-  response.data.sort(function(a,b) {
-    return a.result - b.result;
-  }).forEach(function(check) {
-    var checkNode = document.createElement("p");
-    checkNode.className = "check " + check.status;
-    checkNode.innerHTML = check.job;
-    document.getElementById("checks").appendChild(checkNode);
+var update = function() {
+  axios.get('api/v1/checks?checkfile=https://raw.githubusercontent.com/cloudfoundry/capi-checkman/master/pipeline').then(function(response) {
+    var checks = document.getElementById("checks");
+    checks.innerHTML = '';
+    response.data.sort(function(a,b) {
+      return a.result - b.result;
+    }).map(function(check) {
+      var checkNode = document.createElement("p");
+      checkNode.className = "check " + check.status;
+      checkNode.innerHTML = check.job;
+      return checkNode;
+    }).forEach(function(checkNode) {
+      checks.appendChild(checkNode);
+    });
+  }).catch(function(e) {
+    console.log('error refreshing status', e);
+  }).then(function() {
+    setTimeout(update, 30000);
   });
-});
+};
 
+update();
