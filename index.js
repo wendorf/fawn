e481ef9e140a7b102f76b036d18f7ae163828f05
Dan Wendorf <dwendorf@pivotal.io>
2016-05-08 22:44:03 -0700

    Make more 12-factor-ish

--- index.js
+++ index.js
@@ -55,6 +55,6 @@ app.get('/api/v1/checks', function(req, res){
 
 app.use(express.static('public'));
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log('Example app listening on port 3000!');
 });
